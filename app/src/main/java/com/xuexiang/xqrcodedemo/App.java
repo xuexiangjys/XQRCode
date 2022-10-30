84a7f005b4c4c19f634522e0db43d1e4a35018e7
xuexiangjys <xuexiangjys@163.com>
2022-10-31 00:43:59 +0800

    升级版本

--- app/src/main/java/com/xuexiang/xqrcodedemo/App.java
+++ app/src/main/java/com/xuexiang/xqrcodedemo/App.java
@@ -16,24 +16,17 @@
 
 package com.xuexiang.xqrcodedemo;
 
+import static com.xuexiang.xaop.consts.PermissionConsts.CAMERA;
+
 import android.app.Application;
-import android.content.Context;
 
 import com.xuexiang.xaop.XAOP;
 import com.xuexiang.xaop.annotation.Permission;
-import com.xuexiang.xaop.util.PermissionUtils;
-import com.xuexiang.xpage.AppPageConfig;
 import com.xuexiang.xpage.PageConfig;
-import com.xuexiang.xpage.PageConfiguration;
-import com.xuexiang.xpage.model.PageInfo;
 import com.xuexiang.xqrcode.XQRCode;
 import com.xuexiang.xutil.XUtil;
 import com.xuexiang.xutil.common.StringUtils;
 import com.xuexiang.xutil.tip.ToastUtils;
-
-import java.util.List;
-
-import static com.xuexiang.xaop.consts.PermissionConsts.CAMERA;
 
 /**
  * <pre>
@@ -50,24 +43,14 @@ public class App extends Application {
         XUtil.init(this);
         XUtil.debug(true);
 
-        PageConfig.getInstance().setPageConfiguration(new PageConfiguration() { //页面注册
-            @Override
-            public List<PageInfo> registerPages(Context context) {
-                return AppPageConfig.getInstance().getPages(); //自动注册页面
-            }
-        }).debug("PageLog").enableWatcher(true).init(this);
-
+        PageConfig.getInstance()
+                .debug("PageLog")
+                .init(this);
 
         XAOP.init(this); //初始化插件
         XAOP.debug(true); //日志打印切片开启
         //设置动态申请权限切片 申请权限被拒绝的事件响应监听
-        XAOP.setOnPermissionDeniedListener(new PermissionUtils.OnPermissionDeniedListener() {
-            @Override
-            public void onDenied(List<String> permissionsDenied) {
-                ToastUtils.toast("权限申请被拒绝:" + StringUtils.listToString(permissionsDenied, ","));
-            }
-
-        });
+        XAOP.setOnPermissionDeniedListener(permissionsDenied -> ToastUtils.toast("权限申请被拒绝:" + StringUtils.listToString(permissionsDenied, ",")));
 
         initPermission();
     }
